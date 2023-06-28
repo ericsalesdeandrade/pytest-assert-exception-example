a1faa8dca851b20ed4cf339c140213b52a613160
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-06-28 09:26:41 +0100

    Init commit

---
+++ src/assert_examples.py
@@ -0,0 +1,61 @@
+import os
+import math
+import re
+
+class InvalidEmailError(Exception):
+    """
+    Raised when an email address is invalid
+    """
+    pass
+
+def division(a: int | float, b: int | float) -> float | ZeroDivisionError:
+    """
+    Returns the result of dividing a by b
+    
+    Raises:
+        ZeroDivisionError: If b is 0
+    """
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Division by zero is not allowed")
+    
+def square_root(a: int) -> float | ValueError:
+    """
+    Returns the square root of a
+    
+    Raises:
+        ValueError: If a is negative
+    """
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        raise ValueError("Square root of negative numbers is not allowed")
+    
+def delete_file(filename:str) -> None | FileNotFoundError:
+    """
+    Deletes a file
+    
+    Raises:
+        FileNotFoundError: If the file does not exist
+    """
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {filename} not found")
+    
+def validate_email(email: str) -> bool | InvalidEmailError:
+    """
+    Validates an email address
+    
+    Raises:
+        InvalidEmailError: If the email address is invalid
+    """
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return True
+    else:
+        raise InvalidEmailError("Invalid email address")
+        
+
+
+    
---
+++ tests/unit/test_assert_exceptions.py
@@ -0,0 +1,48 @@
+import pytest
+from src.assert_examples import division, square_root, validate_email, delete_file, InvalidEmailError
+
+def test_division_zero_division_error():
+    """
+    Test that a ZeroDivisionError is raised when the second argument is 0
+    """
+    with pytest.raises(ZeroDivisionError) as excinfo:
+        division(1, 0)
+    assert str(excinfo.value) == "Division by zero is not allowed"
+
+def test_square_root_value_error():
+    """
+    Test that a ValueError is raised when the argument is negative
+    """
+    with pytest.raises(ValueError) as excinfo:
+        square_root(-1)
+    assert str(excinfo.value) == "Square root of negative numbers is not allowed"
+
+def test_delete_file_not_found_error():
+    """
+    Test that a FileNotFoundError is raised when the file does not exist
+    """
+    with pytest.raises(FileNotFoundError) as excinfo:
+        delete_file("non_existent_file.txt")
+    assert str(excinfo.value) == "File non_existent_file.txt not found"
+
+def test_validate_email_value_error():
+    """
+    Test that an InvalidEmailError is raised when the email address is invalid
+    """
+    with pytest.raises(InvalidEmailError) as excinfo:
+        validate_email("invalid_email")
+    assert str(excinfo.value) == "Invalid email address"
+
+def test_division_no_exception_raised():
+    """
+    Test that no exception is raised when the second argument is not 0
+    """
+    try:
+        division(1, 1)
+    except Exception as excinfo:
+        pytest.fail(f"Unexpected exception raised: {excinfo}")
+
+
+
+
+
